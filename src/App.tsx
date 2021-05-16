99723a4c79faa0586eebf9386adeb49a8b005fd8
Rudney R <rudney.un2016@gmail.com>
2021-05-15 23:16:06 -0300

    Application structure

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { GlobalStyle } from './styles/global';
+
+export function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <h1>Ola mundo</h1>
+    </div>
+  );
+}
