e5dc59dd42354f8a76aa6a17a6c1f2c0cf91726d
Rudney R <rudney.un2016@gmail.com>
2021-05-17 21:56:33 -0300

    Creating Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Header } from './components/Header';
+
 import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
-    <div className="App">
+    <>
       <GlobalStyle />
-      <h1>Ola mundo</h1>
-    </div>
+      <Header />
+    </>
   );
 }
