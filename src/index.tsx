d7acb9f807cf097920ea92f96ddc8a41e536b693
Rudney R <rudney.un2016@gmail.com>
2021-05-18 20:51:00 -0300

    Configuring dummy API

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { createServer } from 'miragejs';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+    //Todas as chamadas ao banco de dados serão efetuadas no endereço:
+    //http://localhost:3000/api
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Salario',
+          amount: 1200,
+          type: 'deposit',
+          category: 'Pessoal',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
